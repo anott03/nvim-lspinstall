194c695754c0c121fb87d8ae534ede64c9097364
Amitav Nott <amitav_nott@ryecountryday.org>
2021-01-10 09:52:50 -0500

    fixed rust!

--- scripts/Linux/rust.sh
+++ scripts/Linux/rust.sh
@@ -3,6 +3,6 @@ if [[ -f ~/.local/bin/rust-analyzer ]]
 then
   rm -rf ~/.local/bin/rust-analyzer
 fi
-curl -L https://githubcom/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux -o ~/.local/bin/rust-analyzer &&
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux -o ~/.local/bin/rust-analyzer
 chmod +x ~/.local/bin/rust-analyzer &&
 echo "rust analyzer successfully installed"
